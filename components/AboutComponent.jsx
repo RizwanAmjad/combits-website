5053e26a50a01b931beb7199a24839604def0768
RizwanAmjad <rizwanamjadnov@gmail.com>
2022-05-20 19:52:34 +0500

    Added progress bar for navigation for usability

---
+++ components/AboutComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function AboutComponent(props) {
+  return <div></div>
+}
+
+export default AboutComponent
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-import NavComponent from "../components/NavComponent";
+import NextProgress from "next-progress"
 
-import "../styles/globals.css";
-import "normalize.css";
+import NavComponent from "../components/NavComponent"
+
+import "../styles/globals.css"
+import "normalize.css"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavComponent />
+      <NextProgress delay={300} options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
